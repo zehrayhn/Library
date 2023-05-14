171170c83e4c5225db730b622bd04a8087d6ed06
Aleyna Demir <75533653+aleyynademir@users.noreply.github.com>
2023-05-14 19:31:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNav);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+                findFragmentById(R.id.navHostFragment);
+        NavigationUI.setupWithNavController(bottomNav,navHostFragment.getNavController());
+
+
+    }
+}
