59b5cc4abb6d6deb3ee5bd5c28861f406281b222
talha.ozdemir <talha.ozdemir@blue-chip.com.tr>
2023-06-18 21:17:06 +0300

    user autotantikation

--- app/src/main/java/com/example/library/ProfileFragment.java
+++ app/src/main/java/com/example/library/ProfileFragment.java
@@ -35,88 +35,88 @@ import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
-    String userName;
-    MaterialButton exitButton;
-    private ListView  listAdd,listEnd;
-    private SharedPreferences sharedPreferences;
+String userName;
+MaterialButton exitButton;
+private ListView  listAdd,listEnd;
+private SharedPreferences sharedPreferences;
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        sharedPreferences = getContext().getSharedPreferences("veriler", Context.MODE_PRIVATE);
-        userName = sharedPreferences.getString("username", "no login");;
-        listAdd = view.findViewById(R.id.listAdd);
-        listEnd = view.findViewById(R.id.listEnd);
-        exitButton = view.findViewById(R.id.exitBtn);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
+    View view = inflater.inflate(R.layout.fragment_profile, container, false);
+    sharedPreferences = getContext().getSharedPreferences("veriler", Context.MODE_PRIVATE);
+    userName = sharedPreferences.getString("username", "no login");;
+    listAdd = view.findViewById(R.id.listAdd);
+    listEnd = view.findViewById(R.id.listEnd);
+    exitButton = view.findViewById(R.id.exitBtn);
+    exitButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FirebaseAuth.getInstance().signOut();
 
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.remove("username");
-                editor.apply();
-                Intent intent =new Intent(view.getContext(),LoginActivity.class);
-                startActivity(intent);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.remove("username");
+            editor.apply();
+            Intent intent =new Intent(view.getContext(),LoginActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("users/"+userName+"/addBook");
+    ref.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // Veri değiştiğinde bu metot çağırılır
+            List<String> data = new ArrayList<>();
+            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                String value = snapshot.getValue(String.class);
+                data.add(value);
             }
-        });
+            Log.e("Aley",data.toString());
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, data);
+            listAdd.setAdapter(adapter);
+        }
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("users/"+userName+"/addBook");
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // Veri değiştiğinde bu metot çağırılır
-                List<String> data = new ArrayList<>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String value = snapshot.getValue(String.class);
-                    data.add(value);
-                }
-                Log.e("Aley",data.toString());
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, data);
-                listAdd.setAdapter(adapter);
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Log.e("Firebase", "Veri okuma hatası: " + error.getMessage());
+        }
+
+
+    });
+
+    DatabaseReference ref2 = database.getReference("users/"+userName+"/endBook");
+    ref2.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // Veri değiştiğinde bu metot çağırılır
+            List<String> data = new ArrayList<>();
+            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                String value = snapshot.getValue(String.class);
+                data.add(value);
             }
+            Log.e("Aley",data.toString());
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, data);
+            listEnd.setAdapter(adapter);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("Firebase", "Veri okuma hatası: " + error.getMessage());
-            }
+        }
 
-
-        });
-
-        DatabaseReference ref2 = database.getReference("users/"+userName+"/endBook");
-        ref2.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // Veri değiştiğinde bu metot çağırılır
-                List<String> data = new ArrayList<>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String value = snapshot.getValue(String.class);
-                    data.add(value);
-                }
-                Log.e("Aley",data.toString());
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, data);
-                listEnd.setAdapter(adapter);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("Firebase", "Veri okuma hatası: " + error.getMessage());
-            }
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Log.e("Firebase", "Veri okuma hatası: " + error.getMessage());
+        }
 
 
-        });
+    });
 
 
 
-        return  view;
+    return  view;
 
 
-    }
+}
 
 
 }
